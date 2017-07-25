eefabed877eb1baa9ee8035756ce1e0a0308d877
Jerry <dingzhiwei007@gmail.com>
2017-07-25 12:41:03 -0400

    Starts the FCM

---
+++ fcm/math.py
@@ -0,0 +1,17 @@
+"""
+A small set of functions for doing math operatons.
+"""
+
+# write a function named add that adds two  values
+
+def add(arg1,arg2):
+    """
+    Function that adds two arguments
+    """
+    return arg1 + arg2
+
+def mult(arg1,arg2):
+    """
+    Function that multipy two arguments
+    """
+    return arg1 * arg2
