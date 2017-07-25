a4e5d0128c444cf7d1a50266c4840ade28ccc890
Jerry <dingzhiwei007@gmail.com>
2017-07-25 14:34:33 -0400

    more test data

--- tests/test_fcm.py
+++ tests/test_fcm.py
@@ -10,6 +10,12 @@ import pytest
 def test_add():
     assert fcm.math.add(5,2) == 7 
     assert fcm.math.add(2,5) == 7
-def test_mult():
-    assert fcm.math.mult(5,2) == 10
-    assert fcm.math.mult(2,5) == 10
+
+testdata = [
+    (2, 5, 10),
+    (1, 2, 2 ),
+    (11, 9, 99)
+]
+@pytest.mark.parametrize("a,b,expected", testdata)
+def test_mult(a, b, expected):
+    assert fcm.math.mult(a, b) == expected
